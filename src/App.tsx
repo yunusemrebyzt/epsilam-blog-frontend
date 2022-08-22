197b49979462cb79adc323fb5ec351bab51fce5c
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2022-08-22 11:31:49 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "styles/app.scss";
+import "styles/global.scss";
+import { Provider } from "react-redux";
+import store from "redux/store";
+import "./i18n";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Home />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
