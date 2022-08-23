b27209e6c17478c6c0ac15c528b77c2b8372cfc1
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2022-08-23 13:06:40 +0300

    first commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "../../components/Header";
+import './Home.scss'
 
 
 const Home: React.FC = () => {
     return (
         <div className="">
             <Header/>
+            <div className="container title">
+                <h1>Welcome to the Blog</h1>
+                <p>SOME INSTERESTIONG SUBTITLE FOR BLOG PAGE</p>
+            </div>
+            <div className="mt-5 headerImg d-flex align-items-end justify-content-start">
+                <div className="container headerText  ">
+                    <h3>The Beauty of modern living </h3>
+                    <p>june 24,2022</p>
+                </div>
 
+            </div>
         </div>
 
     );
