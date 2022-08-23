cb7116f29a15cc9ef68e5405db3aa56926391f7d
yunusemrebyzt <69965388+yunusemrebyzt@users.noreply.github.com>
2022-08-23 09:43:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import "styles/app.scss";
 import "styles/global.scss";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
+import React from "react";
 import store from "redux/store";
 import "./i18n";
-
-import Home from "pages";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "pages/Home";
+import Profile from "./pages/Profile";
 
 function App(): JSX.Element {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <Home />
-      </div>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                </Routes>
+            </Router>
+        </Provider>
+    );
 }
+
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "../../components/Header";
+
+
+const Home: React.FC = () => {
+    return (
+        <div className="">
+            <Header/>
+
+        </div>
+
+    );
+};
+
+export default Home;
